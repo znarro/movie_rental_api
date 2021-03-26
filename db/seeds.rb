e406a871235555f52f74bdd4e67ee00c943069d8
Josue Narro <josue.narro.a@gmail.com>
2021-03-26 09:44:12 -0500

    Add data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,72 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-u = User.create!(name: 'Zamir', email: 'zamir@tekton.com')
-y = User.create!(name: 'Simba', email: 'simba@tekton.com')
-Category.create!(name: 'Drama', description: 'Some description')
-Category.create!(name: 'Comedy', description: 'Some description')
-Movie.create!({ name: 'The Godfather',
-               director: 'Francis Ford Coppola',
-               release_date: '14/03/1972',
-               category: Category.first })
-Movie.create!({ name: 'Superbad',
-               director: 'Greg Mottola',
-               release_date: '12/10/2007',
-               user: nil,
-               category: Category.last })
-Movie.create!({ name: 'Sound of Metal',
-               director: 'Darius Marder',
-               release_date: '06/09/2019',
-               user: nil,
-               category: Category.last })
+DATA = {
+  user_keys:
+    ['name', 'email'],
+  users: [
+    ['Harry Potter', 'harry@hogwarts.com'],
+    ['Alf', 'alf@melmack.com'],
+    ['Chewbacca', 'agrrrr@solo.com'],
+    ['Rick Sanchez', 'pickle@rick.com'],
+  ],
+ 
+  category_keys:
+    ['name', 'description'],
+  categories: [
+    ['Drama', 'Sad films'],
+    ['Comedy', 'Just laugh'],
+    ['Superheroes', 'Marvel and DC'],
+    ['Horror', 'Scary movie'],
+  ],
 
+  movie_keys:
+    ['name', 'director', 'release_date', 'user_id', 'category_id'],
+  movies: [
+    ['The Godfather', 'Francis Ford Coppola', '14/03/1972', 1, 1],
+    ['Superbad', 'Greg Mottola', '14/02/2007', nil, 2],
+    ['Star Wars', 'George Lucas', '14/02/1990', nil, 1],
+    ['The Exorcist', 'Some guy', '14/02/1980', 2, 4],
+    ['Justice League', 'Zack Snyder', '14/03/2021', nil, 3],
+    ['Watchmen', 'Zack Snyder', '14/03/2000', 3, 3],
+    ['Asu mare', 'Cachin', '14/03/2000', nil, 4],
+  ]
+}
+
+def make_data
+  make_customers
+  make_categories
+  make_movies
+end
+
+def make_customers
+  DATA[:users].each do |user|
+    new_user = User.new
+    user.each_with_index do |attribute, i|
+      new_user.send("#{DATA[:user_keys][i]}=", attribute)
+    end
+    new_user.save
+  end
+end
+
+def make_categories
+  DATA[:categories].each do |cat|
+    new_cat = Category.new
+    cat.each_with_index do |attribute, i|
+      new_cat.send("#{DATA[:category_keys][i]}=", attribute)
+    end
+    new_cat.save
+  end
+end
+
+def make_movies
+  DATA[:movies].each do |movie|
+    new_movie = Movie.new
+    movie.each_with_index do |attribute, i|
+      new_movie.send("#{DATA[:movie_keys][i]}=", attribute)
+    end
+    new_movie.save
+  end
+end
+
+make_data
