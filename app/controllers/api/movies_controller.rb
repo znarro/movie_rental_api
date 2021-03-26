51dbb3e08707b1a6b88d8bc95c5a77c44e90bb36
Josue Narro <josue.narro.a@gmail.com>
2021-03-25 19:24:53 -0500

    Create Users and Movies controllers

---
+++ app/controllers/api/movies_controller.rb
@@ -0,0 +1,11 @@
+class Api::MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+    render json: @movies
+  end
+  
+  def show
+    @movie = Movie.find(params[:id])
+    render json: @movie
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 u = User.create(name: 'Zamir', email: 'zamir@tekton.com')
+y = User.create(name: 'Simba', email: 'simba@tekton.com')
 Category.create(name: 'Drama', description: 'Some description')
+Category.create(name: 'Comedy', description: 'Some description')
 Movie.create({ name: 'The Godfather',
                director: 'Francis Ford Coppola',
                release_date: '14/03/1972',
                category: Category.first })
+Movie.create({ name: 'Superbad',
+               director: 'Greg Mottola',
+               release_date: '12/10/2007',
+               category: Category.last })
+Movie.create({ name: 'Sound of Metal',
+               director: 'Darius Marder',
+               release_date: '06/09/2019',
+               category: Category.last })
 u.rentals.create(movie: Movie.first, is_rented: true)
+y.rentals.create(movie: Movie.find_by(id: 2), is_rented: true)
+u.rentals.create(movie: Movie.last, is_rented: true)
+
