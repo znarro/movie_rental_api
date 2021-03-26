6c41be9d037e7c79d6ed87ddf46f4b65a404662d
Josue Narro <josue.narro.a@gmail.com>
2021-03-26 04:41:27 -0500

    Fix Movie destroy

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -13,7 +13,7 @@ class Api::MoviesController < ApplicationController
   def create
     @movie = Movie.new(movie_params)
     if @movie.save
-      render json: @movie, status: :created, location: @movie
+      render json: @movie, status: :created
     else
       render json: @movie.errors, status: :unprocessable_entity
     end
